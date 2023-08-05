b3237bd0797a1bd0b2011b34e594f8a8b7da06ad
XCWQW1 <3539757707@qq.com>
2023-08-05 22:41:10 +0800

    增加私聊接口，但是有点小问题会导致框架重启

--- API/api_kook.py
+++ API/api_kook.py
@@ -141,9 +141,9 @@ class KOOKApi:
 
         if request['code'] == 0:
             if msg_type == 1:
-                Log.send(msg_type, send_msg, channel_id, request['data']['msg_id'])
+                Log.send(msg_type, send_msg, user_id, request['data']['msg_id'])
             else:
-                Log.send(msg_type, send_msg, channel_id, request['data']['msg_id'])
+                Log.send(msg_type, send_msg, user_id, request['data']['msg_id'])
             return request['data']['msg_id']
         else:
             print(request)
