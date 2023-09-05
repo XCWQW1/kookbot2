96a2c2f102b4a3120cd99ea6888b9faffb8c160a
XCWQW1 <3539757707@qq.com>
2023-09-05 18:10:59 +0800

    稳！

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import signal
 import sys
 import time
 
-from plugin_system.plugin_transfer import PluginTransferThread
 from API.api_kook import KOOKApi
 from API.api_log import LogSP
 from init.main_init import main_init
@@ -13,8 +12,6 @@ from colorama import init, Fore, Style
 
 # 初始化colorama
 init()
-thread = PluginTransferThread()
-thread.start()
 
 
 async def kook_bot():
