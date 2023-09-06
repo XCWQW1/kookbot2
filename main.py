51ba62ffc3bda034e59b42c2fb7fa7b065de4910
XCWQW1 <3539757707@qq.com>
2023-09-06 09:29:52 +0800

    新增版本号输出

--- main.py
+++ main.py
@@ -22,6 +22,10 @@ async def kook_bot():
 async def main():
     current_time = time.time()
     now_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(current_time))
+    version = 'v1.2.4'
+    version_text = f"[{now_time}] [版本]" + ' ' + '当前框架运行的版本：' + version
+    print(version_text)
+    art_text = text2art('XCBOT')
     art_text = text2art('XCBOT')
 
     # 拆分艺术字的每一行，并在每行前面添加当前时间
